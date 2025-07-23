dd3af946b50477c1f5a096fb8d9190aab8025d99
YAO-EE <128304396+YAO-EE@users.noreply.github.com>
2025-07-23 14:45:26 +0800

    Update finetune_m3ae.sh

--- run_scripts/finetune_m3ae.sh
+++ run_scripts/finetune_m3ae.sh
@@ -1,7 +1,6 @@
 num_gpus=1
 per_gpu_batchsize=16
 
-# === VQA ===
 python main.py with data_root=data/finetune_arrows/ \
  num_gpus=${num_gpus} num_nodes=1 \
  task_finetune_vqa_vqa_rad \
@@ -23,39 +22,7 @@ python main.py with data_root=data/finetune_arrows/ \
 
 python main.py with data_root=data/finetune_arrows/ \
  num_gpus=${num_gpus} num_nodes=1 \
- task_finetune_vqa_medvqa_2019 \
- per_gpu_batchsize=${per_gpu_batchsize} \
- clip16 text_roberta \
- image_size=384 \
- tokenizer=downloaded/roberta-base \
- load_path=downloaded/pretrained/m3ae.ckpt \
- clip_resizedcrop
-
-# === CLS ===
-python main.py with data_root=data/finetune_arrows/ \
- num_gpus=${num_gpus} num_nodes=1 \
- task_finetune_cls_melinda_p_meth_label \
- per_gpu_batchsize=${per_gpu_batchsize} \
- clip16 text_roberta \
- image_size=384 \
- tokenizer=downloaded/roberta-base \
- load_path=downloaded/pretrained/m3ae.ckpt \
- clip_resizedcrop
-
-# === IRTR ===
-python main.py with data_root=data/finetune_arrows/ \
- num_gpus=${num_gpus} num_nodes=1 \
- task_finetune_irtr_roco get_recall_metric=True \
- pwdper_gpu_batchsize=${per_gpu_batchsize} \
- clip16 text_roberta \
- image_size=288 \
- tokenizer=downloaded/roberta-base \
- test_only=True \
- load_path=downloaded/pretrained/m3ae.ckpt
-
-python main.py with data_root=data/finetune_arrows/ \
- num_gpus=${num_gpus} num_nodes=1 \
- task_finetune_irtr_roco get_recall_metric=False \
+ task_finetune_vqa_ovqa \
  per_gpu_batchsize=${per_gpu_batchsize} \
  clip16 text_roberta \
  image_size=384 \
