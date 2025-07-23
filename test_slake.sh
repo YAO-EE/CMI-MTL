064adbd58e62677bcd34491183b1c4006cf0c115
YAO-EE <128304396+YAO-EE@users.noreply.github.com>
2025-07-23 15:24:57 +0800

    Add files via upload

---
+++ test_slake.sh
@@ -0,0 +1,17 @@
+num_gpus=1
+per_gpu_batchsize=16
+
+# === SLACK ===
+# === Provided Checkpoints ===
+# downloaded/finetuned/vqa/slack/m3ae_finetuned_vqa_slack_84.3.ckpt
+# downloaded/finetuned/vqa/slack/m3ae_finetuned_vqa_slack_83.0.ckpt
+# downloaded/finetuned/vqa/slack/m3ae_finetuned_vqa_slack_82.5.ckpt
+python main.py with data_root=data/finetune_arrows/ \
+ num_gpus=${num_gpus} num_nodes=1 \
+ task_finetune_vqa_slack \
+ per_gpu_batchsize=${per_gpu_batchsize} \
+ clip16 text_roberta \
+ image_size=384 \
+ test_only=True \
+ tokenizer=/mnt/3bb01a96-4d48-4134-9dcc-1e1cdd11daa3/zxy/M3AE-master/downloaded/roberta-base \
+ load_path=/mnt/3bb01a96-4d48-4134-9dcc-1e1cdd11daa3/zxy/M3AE-master/downloaded/finetuned/vqa/slack/m3ae_finetuned_vqa_slack_84.3.ckpt
